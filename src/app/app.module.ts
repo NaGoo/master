13c6d606c13fb546a5bf78d622fa3175314d2666
NaGoo <64887925+NaGoo@users.noreply.github.com>
2020-07-29 09:33:35 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,29 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule  } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import {MatSelectModule} from '@angular/material/select'; 
-
 import {MatCheckboxModule} from '@angular/material/checkbox'; 
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatInputModule} from '@angular/material/input'; 
 import { AppComponent }      from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceComponent } from './service/service.component';
 import { ClientMasterComponent } from './client-master/client-master.component';
 // import { ClientMasterComponent } from './client-master/client-master.component';
-
 @NgModule({
   imports: [
-    BrowserModule,MatToolbarModule,MatSelectModule,
+    BrowserModule,MatToolbarModule,MatSelectModule,HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,MatButtonModule,MatInputModule,MatCheckboxModule
-    
-    
   ],
   declarations: [
     AppComponent,
     ServiceComponent,
     ClientMasterComponent,
     // ClientMasterComponent,
-    
   ],
   providers: [],
   bootstrap: [ AppComponent ]
