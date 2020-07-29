2c788943d730c2cce93dd542e3b60c62f8fd810a
NaGoo <64887925+NaGoo@users.noreply.github.com>
2020-07-29 09:35:01 +0530

    Update client-master.component.ts

--- src/app/client-master/client-master.component.ts
+++ src/app/client-master/client-master.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import {  FormControl,FormGroup,Validators, FormControlName } from '@angular/forms';
-
+import { Component } from '@angular/core';
+import { HttpClientModule, HttpClient,HttpHeaders } from '@angular/common/http';
+import { FormGroup,Validators,FormControl } from '@angular/forms';
+// import {MediaObserver,MediaChange} from '@angular/flex-layout';
+import {Subscription } from 'rxjs';
 @Component({
   selector: 'app-client-master',
   templateUrl: './client-master.component.html',
   styleUrls: ['./client-master.component.css']
 })
-export class ClientMasterComponent implements OnInit {
-
-
-  client=new FormGroup({
-    name:new FormControl('',Validators.required)
-  })
-  constructor() { }
-
-  ngOnInit(): void {
+export class ClientMasterComponent  {
+  device:boolean;
+  constructor( private http: HttpClient  ){
   }
-
-  onSubmit(a){
-    console.log(a);
-  }
-get name(){
-  return this.client.get('name')
+ client=new FormGroup({
+  name:new FormControl('',Validators.required)
+})
+onSubmit(e)
+  {
+    const header = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiaGFyaXRoYSBwIiwiaWQiOjEsImVtYWlsIjoiaGFyaXRoYXBuYWlyMjIwNUBnbWFpbC5jb20iLCJtb2JpbGVObyI6Ijk0ODk0NTIzNDgiLCJSb2xlTmFtZSI6IkFkbWluIiwidmVuZG9ySWQiOm51bGwsImlhdCI6MTU5NTg2MDMxOSwiZXhwIjoxNTk4NDUyMzE5fQ.NDvejKkotVpGzqMm5HMxaayxcUqgVTFoXuAW8cqJ2Fc'
 }
+const request = {                                                                                                                                                                                 
+  headers: new HttpHeaders(header), 
+};
+    this.client.reset();
+    this.http.post ('http://localhost:3000/api/client-masters', e,request)
+    .subscribe((result)=>{
+      console.log(result);
+    console.log(e);
+    })
+  }
+ get name(){return this.client.get("name")}
 }
