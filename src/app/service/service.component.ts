f3612db36e2df5286729076ce6a07892ed40dcb8
NaGoo <64887925+NaGoo@users.noreply.github.com>
2020-08-04 09:32:55 +0530

    Update service.component.ts

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {  FormControl,FormGroup,Validators } from '@angular/forms';
 import { HttpClientModule, HttpClient,HttpHeaders } from '@angular/common/http';
-import { stringify } from 'querystring';
 interface service {
   value: string;
   viewValue: string;
 }
+const header = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiaGFyaXRoYSBwIiwiaWQiOjEsImVtYWlsIjoiaGFyaXRoYXBuYWlyMjIwNUBnbWFpbC5jb20iLCJtb2JpbGVObyI6Ijk0ODk0NTIzNDgiLCJSb2xlTmFtZSI6IkFkbWluIiwidmVuZG9ySWQiOm51bGwsImlhdCI6MTU5NTg2MDMxOSwiZXhwIjoxNTk4NDUyMzE5fQ.NDvejKkotVpGzqMm5HMxaayxcUqgVTFoXuAW8cqJ2Fc'
+}
+  const request = {                                                                                                                                                                                 
+  headers: new HttpHeaders(header), 
+};
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
   styleUrls: ['./service.component.css']
 })
-export class ServiceComponent {
+export class ServiceComponent implements OnInit{
+  client = [];
+  location = [];
+  vehicle = [];
   categories: service[] = [
     {value: 'ON_SPOT_REPAIRS', viewValue: 'ON_SPOT_REPAIRS'}, 
     {value: 'TOWING', viewValue: 'TOWING'},
@@ -34,31 +45,27 @@ export class ServiceComponent {
   ];
   constructor( private http: HttpClient  ){
   }
+  ngOnInit(): void {
+    this.getClient();
+    this.getVehicle();
+    this.getLocation();
+  }
     service=new FormGroup({
-    category:new FormControl('',Validators.required),
+    category : new FormControl('',Validators.required),
     serviceCode : new FormControl('',Validators.required),
-    clientId : new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$")]),
-    locationId : new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$")]),
-    vehicleId : new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$")]),
+    clientId : new FormControl(''),
+    locationId : new FormControl(''),
+    vehicleId : new FormControl(''),
     leadTime : new FormControl('',Validators.required),
     intervalTime : new FormControl('',Validators.required),
     description : new FormControl('',Validators.required),
-    name : new FormControl('',Validators.required),
+    name:new FormControl('',[Validators.required,Validators.pattern("[a-zA-Z]*")]),
     dayCharge : new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$")]),
     nightCharge : new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$")]),
     tax : new FormControl('',[Validators.required, Validators.pattern("^.[0-9]*$")]),
     displayToCustomer : new FormControl('',Validators.required)})
     onSubmit(e)
     {
-      const header = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiaGFyaXRoYSBwIiwiaWQiOjEsImVtYWlsIjoiaGFyaXRoYXBuYWlyMjIwNUBnbWFpbC5jb20iLCJtb2JpbGVObyI6Ijk0ODk0NTIzNDgiLCJSb2xlTmFtZSI6IkFkbWluIiwidmVuZG9ySWQiOm51bGwsImlhdCI6MTU5NTg2MDMxOSwiZXhwIjoxNTk4NDUyMzE5fQ.NDvejKkotVpGzqMm5HMxaayxcUqgVTFoXuAW8cqJ2Fc'
-  }
-  const request = {                                                                                                                                                                                 
-    headers: new HttpHeaders(header), 
-  };
       this.service.reset();
       var array:any=JSON.stringify({"category":String(e.category),
       "serviceCode":String(e.serviceCode),
@@ -76,7 +83,25 @@ export class ServiceComponent {
       console.log(array);
       this.http.post ('http://localhost:3000/api/service-masters', array,request)
       .subscribe((result)=>{
-        console.log(result);
+        console.log(result);  
+    })
+  }
+  getClient(){
+    return this.http.get<any>('http://localhost:3000/api/client-masters',request)
+    .subscribe((res)=>{
+      this.client = res;
+    })
+  }
+    getVehicle(){
+      return this.http.get<any>('http://localhost:3000/api/vehicle-masters',request)
+      .subscribe((res)=>{
+      this.vehicle = res;
+      })
+    }
+    getLocation(){
+      return this.http.get<any>('http://localhost:3000/api/locations',request)
+      .subscribe((res)=>{
+      this.location = res;
       })
     }
  get category(){return this.service.get('category')}
