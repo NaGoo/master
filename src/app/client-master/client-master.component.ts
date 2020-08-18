52b2f1830cf770460e1d1906113ad480e1db3807
NaGoo <64887925+NaGoo@users.noreply.github.com>
2020-08-18 16:22:41 +0530

    Update client-master.component.ts

--- src/app/client-master/client-master.component.ts
+++ src/app/client-master/client-master.component.ts
@@ -50,15 +50,12 @@ onSubmit(e)
     .subscribe((result)=>{
     console.log(result);
     this.get();
-    this.isCreated =true;
     this.isExist = false;
     console.log(e); 
     this.client.reset(); 
     },
     error =>{
-      this.isCreated=false;
       if(error.status==409)
-      this.isCreated =false;
       this.isExist=true;
     })
     this.client.reset(); 
